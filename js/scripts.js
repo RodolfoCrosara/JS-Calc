48680102b1af6d75029a95b434a9f8df4d3f1d68
Alunos - Senai 6.02 <alunos@educ602.local>
2022-10-29 17:06:04 -0300

    finalizando projeto da calculadora

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ class Calculator {
     //Todas operações da calculadora
     processOperation(operation) {
         //verificar se current está vazio
-        if(this.currentOperationText.innerText === "") {
+        if(this.currentOperationText.innerText === "" && operation !== "C") {
             // mudar operação
             if(previousOperationText.innerText !== "") {
                 this.changeOperation(operation);
@@ -51,7 +51,19 @@ class Calculator {
             case "*":
                 operationValue = previous * current;
                 this.updateScreen(operationValue, operation, current, previous);
-                break;   
+                break;
+            case "DEL":
+                this.processDelOperation();
+                break;
+            case "CE":
+                this.processClearCurrentOperation();
+                break;
+            case "C":
+                this.processClearOperation();
+                break;
+            case "=":
+                this.processEqualsOperation();
+                break;
             default:
                 return;
         }
@@ -76,6 +88,7 @@ class Calculator {
             //adicionar valor atual no valor anterior
             this.previousOperationText.innerText = `${operationValue} ${operation}`;
             this.currentOperationText.innerText = "";
+            
         }
     }
 
@@ -90,6 +103,28 @@ class Calculator {
 
         this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation;
     }
+    //deletar ultimo digito
+    processDelOperation() {
+        this.currentOperationText.innerText = this.currentOperationText.innerText.slice(0, -1);
+    }
+    //limpar operação atual
+    processClearCurrentOperation() {
+        this.currentOperationText.innerText = "";
+    }
+    //limpa todos os valores
+    processClearOperation() {
+        this.currentOperationText.innerText = "";
+        this.previousOperationText.innerText = "";
+    }
+    //mostrar o resultado total
+    processEqualsOperation() {
+        const operation = this.currentOperationText.innerText.split(" ")[1];
+        
+        this.processOperation(operation);
+        
+        
+    }
+
 }
 
 const calc = new Calculator(previousOperationText, currentOperationText);
